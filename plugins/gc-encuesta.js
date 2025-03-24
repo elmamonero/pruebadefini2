e7a8df97be9ca6b323c1956127f74e06132ead96
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-24 18:14:44 -0500

    Actualizar gc-encuesta.js

--- plugins/gc-encuesta.js
+++ plugins/gc-encuesta.js
@@ -5,19 +5,19 @@ Shadow Code*/
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 
     if (!args[0]) {
-        conn.reply(m.chat, `*[ ℹ️ ] Ingrese un texto para iniciar la encuesta.*\n\n*[ 💡 ] Ejemplo:*\n${usedPrefix + command} *texto|texto2*`, m);
+        conn.reply(m.chat, `*🐱 Ingrese un texto para iniciar la encuesta.*\n\n*[ 💡 ] Ejemplo:*\n${usedPrefix + command} *texto|texto2*`, m);
         return;
     }
 
     if (!text.includes('|')) {
-        conn.reply(m.chat, `*[ ⚠️ ] Separe las opciones de la encuesta con \`|\`*\n\n*[ 💡 ] Ejemplo:*\n${usedPrefix + command} *texto|texto2*`, m);
+        conn.reply(m.chat, `*⚠️ Separe las opciones de la encuesta con \`|\`*\n\n*💡 Ejemplo:*\n${usedPrefix + command} *texto|texto2*`, m);
         return;
     }
 
     let opciones = [...new Set(text.split('|'))].map(opcion => [opcion]); // Elimina duplicados
 
     if (opciones.length < 2) {
-        conn.reply(m.chat, `*[ ⚠️️ ] Debe haber al menos dos opciones diferentes en la encuesta.*`, m);
+        conn.reply(m.chat, `*⚠️️ Debe haber al menos dos opciones diferentes en la encuesta.*`, m);
         return;
     }
 
