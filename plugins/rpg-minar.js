94183fcd4cf4be8b633d6acfd39dcfce21d252f4
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-24 18:11:17 -0500

    Actualizar rpg-minar.js

--- plugins/rpg-minar.js
+++ plugins/rpg-minar.js
@@ -5,12 +5,12 @@ let user = global.db.data.users[m.sender]
 let coin = `${pickRandom([20, 5, 7, 8, 30, 35, 40, 45, 50, 55, 60])}` * 1; let emerald = `${pickRandom([1, 5, 7, 8])}` * 1; let iron = `${pickRandom([5, 6, 7, 9, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80])}` * 1; let gold = `${pickRandom([20, 5, 7, 8, 88, 40, 50])}` * 1; let coal = `${pickRandom([20, 5, 7, 8, 88, 40, 50, 80, 70, 60, 100, 120, 600, 700, 64])}` * 1; let stone = `${pickRandom([200, 500, 700, 800, 900, 4000, 300])}` * 1
 
 let time = global.db.data.users[m.sender].lastmiming + 600000
-if (new Date - global.db.data.users[m.sender].lastmiming < 600000) return conn.reply(m.chat, `*[ ⏳ ] Debes esperar \`${msToTime(time - new Date())}\` para volver a minar*`, m, )
+if (new Date - global.db.data.users[m.sender].lastmiming < 600000) return conn.reply(m.chat, `Debes esperar *${msToTime(time - new Date())}* para volver a minar`, m, )
 
 let hasil = Math.floor(Math.random() * 1000)
-let info = `[ ⛏️ ] *Te has adentrando en lo profundo de las cuevas*
+let info = `🐈 *Te has adentrando en lo profundo de las cuevas*
 
-> ᥆ᑲ𝗍ᥙ᥎іs𝗍ᥱ ᥣ᥆s sіgᥙіᥱᥒ𝗍ᥱs rᥱᥴᥙrs᥆s:
+> Obtuviste:
 
 💫 *Exp:* ${hasil}
 💎 *Diamantes:* ${coin}
