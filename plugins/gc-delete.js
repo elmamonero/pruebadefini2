dbcdb54775c7697528fd4544b37e6f63440a1bc6
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-24 18:13:09 -0500

    Actualizar gc-delete.js

--- plugins/gc-delete.js
+++ plugins/gc-delete.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
 	
-if (!m.quoted) return conn.reply(m.chat, `*[ ℹ️ ] Responde al mensaje que deseas eliminar.*`, m)
+if (!m.quoted) return conn.reply(m.chat, `*🐱 Responde al mensaje que deseas eliminar.*`, m)
 try {
 let delet = m.message.extendedTextMessage.contextInfo.participant
 let bang = m.message.extendedTextMessage.contextInfo.stanzaId
