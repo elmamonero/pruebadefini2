eb2f65b57333b7b65b29bb3389554381cfa8fab5
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-29 17:14:52 -0500

    Actualizar fun-personalidad.js

--- plugins/fun-personalidad.js
+++ plugins/fun-personalidad.js
@@ -31,7 +31,7 @@ return list[Math.floor(Math.random() * list.length)]
 
 let handler = async (m, { conn, command, text }) => {
 
-    if (!text) return m.reply(`*[ ℹ️ ] Ingrese el nombre de alguna persona.*\n\n*[ 💡 ] Ejemplo:* .personalidad Shadow`)
+    if (!text) return m.reply(`*🧡 Ingrese el nombre de alguna persona.*\n\n*🐈 Ejemplo:* .personalidad Shadow`)
 
   let personalidad = `
 ┏━━°❀❬ *PERSONALIDAD}* ❭❀°━━┓
