12154eb6a5a280e872e6a356ba25edc3a84463c6
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-29 14:31:20 -0500

    Actualizar sticker-qc.js

--- plugins/sticker-qc.js
+++ plugins/sticker-qc.js
@@ -6,8 +6,8 @@ if (args.length >= 1) {
 text = args.slice(0).join(" ");
 } else if (m.quoted && m.quoted.text) {
 text = m.quoted.text;
-} else return conn.reply(m.chat, '*🧡 Agrega un texto.*', m);
-if (!text) return conn.reply(m.chat, '*🧡 Agrega un texto.*', m);
+} else return conn.reply(m.chat, '*🧡 Agrega un texto.*', m, rcanal);
+if (!text) return conn.reply(m.chat, '*🧡 Agrega un texto.*', m, rcanal);
 const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender; 
 const mentionRegex = new RegExp(`@${who.split('@')[0].replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}\\s*`, 'g');
 const mishi = text.replace(mentionRegex, '');
