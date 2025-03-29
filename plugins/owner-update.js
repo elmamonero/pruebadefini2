4dc28755e377102295729e53084f9813a34e38fe
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-29 13:47:59 -0500

    Actualizar owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -21,8 +21,8 @@ let handler = async (m, { conn, text }) => {
   try {
     let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : '')).toString().trim() // Ejecuta git pull y limpia espacios
     let mensaje = stdout.includes('Already up to date') 
-      ? '*[ ℹ️ ] El repositorio del bot está actualizado.*' 
-      : '*[ ℹ️ ] Se actualizó con éxito el repositorio del bot.*\n\n' + stdout
+      ? '*${emojis} El repositorio del bot está actualizado.*' 
+      : '*{emojis} REPOSITORIO ACTUALIZADO*\n\n' + stdout
 
     await conn.reply(m.chat, mensaje, m) // Envía el mensaje con el resultado
     await m.react('✅') // Reacción de éxito
