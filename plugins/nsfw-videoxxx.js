4c78eff3c3d85484c1cece3fb626bc235f2ea948
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-29 15:15:59 -0500

    Actualizar nsfw-videoxxx.js

--- plugins/nsfw-videoxxx.js
+++ plugins/nsfw-videoxxx.js
@@ -7,7 +7,7 @@ let rvid = global.vidxxx[Math.floor(Math.random() * global.vidxxx.length)];
 
 conn.sendMessage(m.chat, { 
         video: { url: rvid }, 
-        caption: '🍒 ¡Disfruta Del Video!', 
+        caption: '😼 ¡Disfruta Del Video!', 
         footer: dev, 
         buttons: [
             {
