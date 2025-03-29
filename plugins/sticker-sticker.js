d9716b5d95e9d122a66be95847f612b4e8b59b61
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-29 14:11:27 -0500

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -8,10 +8,10 @@ try {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || q.mediaType || ''
 if (/webp|image|video/g.test(mime)) {
-if (/video/g.test(mime)) if ((q.msg || q).seconds > 10) return m.reply(`⚠️ El video no puede durar mas de 10 segundos*`)
+if (/video/g.test(mime)) if ((q.msg || q).seconds > 10) return m.reply(`${warn} El video no puede durar mas de 10 segundos*`)
 let img = await q.download?.()
 
-if (!img) return conn.reply(m.chat, `🧡 Responde al video o imagen con el comando*`, m)
+if (!img) return conn.reply(m.chat, `*${emojis} Responde al video o imagen con el comando*`, m)
 
 let out
 try {
@@ -29,7 +29,7 @@ stiker = await sticker(false, out, global.packsticker, global.authsticker)
 } else if (args[0]) {
 if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packsticker, global.authsticker)
 
-else return m.reply(`*🐈 El url es incorrecto*`)
+else return m.reply(`*${mistake} El url es incorrecto*`)
 
 }
 } catch (e) {
@@ -38,7 +38,7 @@ if (!stiker) stiker = e
 } finally {
 if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: botname, body: `Morchi Bot MD`, mediaType: 2, sourceUrl: grupo, thumbnail: icons}}}, { quoted: m })
 
-else return conn.reply(m.chat, `*${emoji2} La conversión ah fallado, responde a un vídeo, imagen o gif lo cual será convertido en sticker.*`, m, rcanal)
+else return conn.reply(m.chat, `*${emojis} La conversión ah fallado, responde a un vídeo, imagen o gif lo cual será convertido en sticker.*`, m, rcanal)
 
 
 }}
