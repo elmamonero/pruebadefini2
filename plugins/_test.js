6b84acfcec6d096c454b9ed972580b0744d9d976
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-30 01:07:42 -0500

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,35 +1,96 @@
-import fetch from 'node-fetch'
-import { sticker } from '../lib/sticker.js'
+import fetch from "node-fetch";
+import axios from 'axios';
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `✳️ Ingresa un enlace de Telegram Stickers.`
-  if (!text.includes('t.me')) return m.reply(`❎ El enlace debe ser de Telegram Stickers.`)
-
-  m.react('⏳') // Reacción de espera
-
+let handler = async (m, { conn, text, usedPrefix, command, args }) => {
   try {
-    let stick = await (await fetch(global.API('fgmods', '/api/downloader/telesticker', { url: text }, 'apikey'))).json()
-    
-    if (!stick.result || stick.result.length === 0) {
-      return m.reply(`❎ No se encontraron stickers en el enlace.`)
+    if (!text) {
+      return conn.reply(m.chat, `🌱 Ejemplo de uso: ytv https://youtube.com/watch?v=Hx920thF8X4`, m);
     }
 
-    let res = stick.result.map(obj => obj.url)
-
-    m.reply(`✅ Enviando Stickers\n\n▢ *Total de stickers:* ${res.length}`)
-
-    for (let i of res) {
-      const stiker = await sticker(false, i, global.packname || 'Shadow Bot', global.author || 'Cristian Escobar')
-      await conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
-      m.react('✅') // Reacción de completado
+    if (!/^(?:https?:\/\/)?(?:www\.|m\.|music\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w\-_]+)\&?/.test(args[0])) {
+      return m.reply(`Enalce inválido`);
     }
-  } catch (error) {
-    m.reply(`❎ Error al descargar los stickers. Intenta nuevamente.`)
+
+    m.react('🕒');
+    let json = await ytdl(args[0]);
+    let limit = 10485760;
+    let size = await getSize(json.url);
+
+    const cap = `\`\`\`⊜─⌈ 📻 ◜YouTube MP4◞ 📻 ⌋─⊜\`\`\`\n≡ 🌿 \`Title\` : ${json.title}\n≡ 🌲 \`URL\` : ${args[0]}\n≡ 🌾 Peso: ${await formatSize(size) || "Desconocido"}`;
+
+    conn.sendFile(m.chat, await (await fetch(json.url)).buffer(), `${json.title}.mp4`, cap, m, null, { asDocument: true, mimetype: "video/mp4" })
+
+    m.react('☑️');
+  } catch (e) {
+ m.reply(e)
   }
+};
+
+handler.help = ['ytv'];
+handler.command = ['ytv2', 'ytv'];
+handler.tags = ['dl'];
+handler.diamond = true;
+
+export default handler;
+
+async function ytdl(url) {
+  const headers = {
+    "accept": "*/*",
+    "accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
+    "sec-ch-ua": "\"Not A(Brand\";v=\"8\", \"Chromium\";v=\"132\"",
+    "sec-ch-ua-mobile": "?1",
+    "sec-ch-ua-platform": "\"Android\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "cross-site",
+    "Referer": "https://id.ytmp3.mobi/",
+    "Referrer-Policy": "strict-origin-when-cross-origin"
+  };
+  const initial = await fetch(`https://d.ymcdn.org/api/v1/init?p=y&23=1llum1n471&_=${Math.random()}`, { headers });
+  const init = await initial.json();
+  const id = url.match(/(?:youtu\.be\/|youtube\.com\/(?:.*v=|.*\/|.*embed\/))([^&?/]+)/)?.[1];
+  const convertURL = init.convertURL + `&v=${id}&f=mp4&_=${Math.random()}`;
+
+  const converts = await fetch(convertURL, { headers });
+  const convert = await converts.json();
+
+  let info = {};
+  for (let i = 0; i < 3; i++) {
+    const progressResponse = await fetch(convert.progressURL, { headers });
+    info = await progressResponse.json();
+    if (info.progress === 3) break;
+  }
+
+  const result = {
+    url: convert.downloadURL,
+    title: info.title
+  };
+  return result;
 }
 
-handler.help = ['telestick']
-handler.tags = ['sticker']
-handler.command = ['tgstick', 'telestick', 'telesticker', 'tgsticker']
+async function formatSize(bytes) {
+    const units = ['B', 'KB', 'MB', 'GB', 'TB'];
+    let i = 0;
+    bytes = Number(bytes);
 
-export default handler
+    if (isNaN(bytes)) {
+        return 'Tamaño de bytes inválido'
+    }
+
+    while (bytes >= 1024 && i < units.length - 1) {
+        bytes /= 1024;
+        i++;
+    }
+
+    return `${bytes.toFixed(2)} ${units[i]}`;
+}
+
+async function getSize(url) {
+  try {
+      const response = await axios.head(url);
+      const contentLength = response.headers['content-length'];
+      return contentLength ? parseInt(contentLength, 10) : null;
+  } catch (error) {
+      return error;
+  }
+}
