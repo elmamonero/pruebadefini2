fe1290a8710ac990fd658965691c47ef456e97de
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-30 00:43:13 -0500

    Actualizar main-menunsfw.js

--- plugins/main-menunsfw.js
+++ plugins/main-menunsfw.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 const handler = async (m, {conn, usedPrefix, text, isPrems}) => {
 
   try {
-    const img = './media/menus/catalgo.jpg';
+    const img = './media/catalgo.jpg';
     const more = String.fromCharCode(8206);
     const readMore = more.repeat(850);
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
