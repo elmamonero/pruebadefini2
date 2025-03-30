404637013d55b5ee7de826d0b7e79006f05cebfa
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-30 00:10:17 -0500

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -284,7 +284,7 @@ ${readMore}
   }
 };
 
-handler.command = /^(menu|menú|memu|memú|help|info|comandos|2help|menu1.2|ayuda|commands|commandos|cmd)$/i;
+handler.command = /^(tesm)$/i;
 handler.fail = null;
 
 export default handler;
