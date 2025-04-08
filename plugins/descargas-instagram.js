547ab9573d5d7b4127c0190c6f59c161aa9df40a
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-04-08 17:58:33 -0500

    Actualizar descargas-instagram.js

--- plugins/descargas-instagram.js
+++ plugins/descargas-instagram.js
@@ -11,7 +11,7 @@ let handler = async (m, { args, conn }) => {
     let data = res.data
     for (let media of data) {
       await new Promise(resolve => setTimeout(resolve, 2000))
-      await conn.sendFile(m.chat, media.url, 'instagram.mp4', '*_DESCARGAS - INSTAGRAM_*\n\n> * [ 🍢 ] Vídeo de Instagram descargado correctamente por Shadow Bot - MD*')
+      await conn.sendFile(m.chat, media.url, 'instagram.mp4', '*_DESCARGAS - INSTAGRAM_*\n\n> ${dev}')
     }
   } catch {
     await m.react('❌')
