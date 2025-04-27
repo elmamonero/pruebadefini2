9933b5e14de36f99fb0384a9517b241b3f6afca0
elmamonero <debastado0@gmail.com>
2025-04-27 18:25:57 -0400

    Update descargas-ytmp4.js

--- plugins/descargas-ytmp4.js
+++ plugins/descargas-ytmp4.js
@@ -1,29 +1,25 @@
-/* [🍭] YOUTUBE VIDEO
-- By WillZek 
-*/
+import fetch from 'node-fetch';  
+import fg from 'senna-fg';  
 
-import fetch from 'node-fetch';
-import fg from 'senna-fg';
+let handler = async(m, { conn, args, text }) => {  
+    if (!text) return m.reply(`🍭 Ingresa Un Link De YouTube\n> *Ejemplo:* https://youtube.com/shorts/ZisXJqH1jtw?si=0RZacIJU5zhoCmWh`);  
 
-let handler = async(m, { conn, args, text }) => {
+    m.react(rwait);  
 
-if (!text) return m.reply(`🍭 Ingresa Un Link De YouTube\n> *Ejemplo:* https://youtube.com/shorts/ZisXJqH1jtw?si=0RZacIJU5zhoCmWh`);
+    let data = await fg.ytmp4(text);  
+    let url = data.dl_url;  
 
-m.react(rwait);
+    if (!url) return m.reply('《✧》Hubo un error al intentar acceder al link.\n> Si el problema persiste, reportalo en el grupo de soporte.');  
 
-let data = await fg.ytmp4(text);
-let url = data.dl_url;
+    await conn.sendMessage(m.chat, {  
+        video: { url: url },  
+        mimetype: "video/mp4",  
+        caption: `${dev}`,  
+    }, { quoted: m });  
+    
+    m.react(done);  
+}  
 
-if (!url) return m.reply('《✧》Hubo un error al intentar acceder al link.\n> Si el problema persiste, reportalo en el grupo de soporte.');
+handler.command = ['ytv', 'ytmp4', 'ymp4'];  
 
-await conn.sendMessage(m.chat, {
-      video: { url: url },
-      mimetype: "video/mp4",
-      caption: `${dev}`,
-    }, { quoted: m });
-    m.react(done);
- }
-
-handler.command = ['ytv', 'ytmp4', 'ymp4']
-
-export default handler;
+export default handler;  
