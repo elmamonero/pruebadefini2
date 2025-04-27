0b758ead4674e044cb4cec16674c0016e553c160
elmamonero <debastado0@gmail.com>
2025-04-27 17:44:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ const require = createRequire(__dirname);
 const { name, description, author, version } = require(join(__dirname, './package.json'));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
-say('Morchiyara\nbot', {
+say('Pantheon\nbot', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']
 });
-say(`Developed by Pantheon`, {
+say(`Developed By Pantheon`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']
