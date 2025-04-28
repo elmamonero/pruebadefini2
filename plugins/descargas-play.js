66b5b059fd9e7dfcff113505489871090a15c8bf
elmamonero <debastado0@gmail.com>
2025-04-28 08:25:52 -0400

    Update descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -14,12 +14,12 @@ const handler = async (m, { conn, args, usedPrefix }) => {
         let video = searchResults[0];
         let thumbnail = await (await fetch(video.miniatura)).buffer();
 
-        let messageText = `\`DESCARGAS - PLAY\`\n\n`;
-        messageText += `${video.titulo}\n\n`;
-        messageText += `*⌛ Duración:* ${video.duracion || 'No disponible'}\n`;
-        messageText += `*👤 Autor:* ${video.canal || 'Desconocido'}\n`;
-        messageText += `*📆 Publicado:* ${convertTimeToSpanish(video.publicado)}\n`;
-        messageText += `*🖇️ Url:* ${video.url}\n`;
+        let messageText = `\```◜YouTube - Download◞```\`\n\n`;
+        messageText += `*${video.titulo}*\n\n`;
+        messageText += `≡ *⏳ `Duración`*  ${video.duracion || 'No disponible'}\n`;
+        messageText += `≡ *🌴 `Autor`* ${video.canal || 'Desconocido'}\n`;
+        messageText += `≡ *📆 `Publicado`* ${convertTimeToSpanish(video.publicado)}\n`;
+        messageText += `≡ *🌵 `Url`* ${video.url}\n`;
 
         await conn.sendMessage(m.chat, {
             image: thumbnail,
