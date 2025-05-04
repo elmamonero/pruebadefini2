7b77b9a318fd767013ff10e9e71fa24f421d9d6e
elmamonero <debastado0@gmail.com>
2025-05-04 16:06:47 -0400

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -12,7 +12,9 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isRowner, i
 
   if (bot.antiPrivate && !isOwner && !isRowner && !isMods) {
     await m.reply(
-      `> 《★》@${m.sender.split`@`[0]} Está Prohibido Escribir Al Privado Del Bot.\n> Si Deseas Probar Nuestro Bot Sigue El Canal Oficial : ${channel}`,
+      `> 《★》@${m.sender.split`@`[0]} Está prohibido escribir al privado del bot.\n` +
+      `> Para probar el bot, únete al grupo oficial aquí: https://chat.whatsapp.com/HvDCvNqXSiW19MFXJmWhoF\n` +
+      `> Para seguir nuestro canal oficial, haz clic aquí: ${channel}`,
       false,
       { mentions: [m.sender] }
     );
