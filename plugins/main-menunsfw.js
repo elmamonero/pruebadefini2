c596336111fb607f20257cee99d928928e0aeb40
elmamonero <debastado0@gmail.com>
2025-05-09 22:15:50 -0400

    Update main-menunsfw.js

--- plugins/main-menunsfw.js
+++ plugins/main-menunsfw.js
@@ -4,21 +4,68 @@ let img = 'https://files.catbox.moe/16bm7g.jpg'
 let texto = `• 𝐌𝐄𝐍𝐔 +18 •
 「 🔞 *COMANDOS NOPOR* 🔞 」
 
-║➤ ❤️‍🔥 _.xnxxsearch_ (texto)
-║➤ ❤️‍🔥 _.xnxxdl_ (link)
-║➤ ❤️‍🔥 _.phsearch_ (texto)
-║➤ ❤️‍🔥 _.rule34_ (texto)
-║➤ ❤️‍🔥 _.xvideosearch_ (texto)
-║➤ ❤️‍🔥 _.xvideosdl_ (link)
-║➤ ❤️‍🔥 _.hentaisearch_ (texto)
-║➤ ❤️‍🔥 _.fuck_ (@tag)
-║➤ ❤️‍🔥 _.fuck2_ (@tag)
-║➤ ❤️‍🔥 _.cum_ (@tag)
-║➤ ❤️‍🔥 _.follar_ (@tag)
-║➤ ❤️‍🔥 _.penetrar_ (@tag)
-║➤ ❤️‍🔥 _.horny_ (@tag)
-║➤ ❤️‍🔥 _.violar_ (@tag)
-║➤ ❤️‍🔥 _.sixnine_ (@tag)`
+𓂂𓏸  𐅹੭੭   *\`ᑲᥙsᥴᥲძ᥆rᥱs\`* 🪱 ᦡᦡ
+ര ׄ⃟🪱˚ .xnxxsearch *texto*
+ര ׄ⃟🪱˚ .xvsearch *texto*
+ര ׄ⃟🪱˚ .phsearch *texto*
+ര ׄ⃟🪱˚ .r34 *texto*
+
+𓂂𓏸  𐅹੭੭   *\`ძᥱsᥴᥲrgᥲs\`* 🧋ᦡᦡ
+ര ׄ⃟🧋˚ .xnxxdl *ulr*
+ര ׄ⃟🧋˚ .xvdl *url*
+ര ׄ⃟🧋˚ .phdl *url*
+
+𓂂𓏸  𐅹੭੭   *\`gі𝖿s\`* 🦪 ᦡᦡ
+ര ׄ⃟🦪˚ .follar *@tag*
+ര ׄ⃟🦪˚ .coger *@tag*
+ര ׄ⃟🦪˚ .coger2 *@tag*
+ര ׄ⃟🦪˚ .penetrar *@tag*
+ര ׄ⃟🦪˚ .anal *@tag*
+ര ׄ⃟🦪˚ .sexo *@tag*
+ര ׄ⃟🦪˚ .violar *@tag*
+ര ׄ⃟🦪˚ .rusa *@tag*
+ര ׄ⃟🦪˚ .sixnine *@tag*
+ര ׄ⃟🦪˚ .pies *@tag*
+ര ׄ⃟🦪˚ .mamada *@tag*
+ര ׄ⃟🦪˚ .lickpussy *@tag*
+ര ׄ⃟🦪˚ .grabboobs *@tag*
+ര ׄ⃟🦪˚ .suckboobs *@tag*
+ര ׄ⃟🦪˚ .cum *@tag*
+ര ׄ⃟🦪˚ .fap *@tag*
+ര ׄ⃟🦪˚ .manosear *@tag*
+ര ׄ⃟🦪˚ .lesbianas *@tag*
+
+𓂂𓏸  𐅹੭੭   *\`ᥴ᥆ᥒ𝗍ᥱᥒіძ᥆\`* 🍒 ᦡᦡ
+ര ׄ⃟🍒˚ .pack
+ര ׄ⃟🍒˚ .pack2
+ര ׄ⃟🍒˚ .pack3
+ര ׄ⃟🍒˚ .videoxxx
+ര ׄ⃟🍒˚ .videoxxx2
+ര ׄ⃟🍒˚ .randomxxx
+ര ׄ⃟🍒˚ .nsfwloli
+ര ׄ⃟🍒˚ .nsfwfoot
+ര ׄ⃟🍒˚ .nsfwass
+ര ׄ⃟🍒˚ .nsfwbdsm
+ര ׄ⃟🍒˚ .nsfwcum
+ര ׄ⃟🍒˚ .nsfwero
+ര ׄ⃟🍒˚ .nsfwfemdom
+ര ׄ⃟🍒˚ .nsfwglass
+ര ׄ⃟🍒˚ .nsfworgy
+ര ׄ⃟🍒˚ .yuri
+ര ׄ⃟🍒˚ .yuri2
+ര ׄ⃟🍒˚ .yaoi
+ര ׄ⃟🍒˚ .yaoi2
+ര ׄ⃟🍒˚ .panties
+ര ׄ⃟🍒˚ .tetas
+ര ׄ⃟🍒˚ .booty
+ര ׄ⃟🍒˚ .ecchi
+ര ׄ⃟🍒˚ .furro
+ര ׄ⃟🍒˚ .hentai
+ര ׄ⃟🍒˚ .trapito
+ര ׄ⃟🍒˚ .imagenlesbians
+ര ׄ⃟🍒˚ .pene
+ര ׄ⃟🍒˚ .porno
+ര ׄ⃟🍒˚ .pechos`
 
  conn.sendMessage(m.chat, {
         text: texto,
