b1af29ae2dfea662896077541e7f852e7683ca50
elmamonero <debastado0@gmail.com>
2025-05-21 01:00:55 -0400

    Update herramientas-calc.js

--- plugins/herramientas-calc.js
+++ plugins/herramientas-calc.js
@@ -1,10 +1,13 @@
+// Definir la variable xtools para usar en los mensajes
+const xtools = "🧮";
+
 let handler = async (m, { conn, text }) => {
-  let id = m.chat
-  conn.math = conn.math ? conn.math : {}
+  let id = m.chat;
+  conn.math = conn.math ? conn.math : {};
   if (id in conn.math) {
-    clearTimeout(conn.math[id][3])
-    delete conn.math[id]
-    m.reply('.... ')
+    clearTimeout(conn.math[id][3]);
+    delete conn.math[id];
+    m.reply('.... ');
   }
   let val = text
     .replace(/[^0-9\-\/+*×÷πEe()piPI/]/g, '')
@@ -14,24 +17,28 @@ let handler = async (m, { conn, text }) => {
     .replace(/e/gi, 'Math.E')
     .replace(/\/+/g, '/')
     .replace(/\++/g, '+')
-    .replace(/-+/g, '-')
+    .replace(/-+/g, '-');
+
   let format = val
     .replace(/Math\.PI/g, 'π')
     .replace(/Math\.E/g, 'e')
     .replace(/\//g, '÷')
-    .replace(/\*×/g, '×')
-  try {
-    console.log(val)
-    let result = (new Function('return ' + val))()
-    if (!result) throw result
-    m.reply(`*${format}* = _${result}_`)
-  } catch (e) {
-    if (e == undefined) return m.reply(`*${xtools} Por favor, ingresa una ecuación.*\n*Símbolos compatibles:*\n> -, +, *, /, ×, ÷, π, e, (, )`)
-    return m.reply(`*${xtools} Ingresa un formato correcto, números del \`0-9\` y símbolos* \`-, +, *, /, ×, ÷, π, e, (, )\``)
-  }
-}
-handler.help = ['cal *<ecuacion>*']
-handler.tags = ['tools']
-handler.command = ['cal', 'calc', 'calcular', 'calculadora'] 
+    .replace(/\*×/g, '×');
 
-export default handler
+  try {
+    console.log(val);
+    let result = (new Function('return ' + val))();
+    if (result === undefined) throw result;
+    m.reply(`*${format}* = _${result}_`);
+  } catch (e) {
+    if (e == undefined)
+      return m.reply(`*${xtools} Por favor, ingresa una ecuación.*\n*Símbolos compatibles:*\n> -, +, *, /, ×, ÷, π, e, (, )`);
+    return m.reply(`*${xtools} Ingresa un formato correcto, números del \`0-9\` y símbolos* \`-, +, *, /, ×, ÷, π, e, (, )\``);
+  }
+};
+
+handler.help = ['cal *<ecuacion>*'];
+handler.tags = ['tools'];
+handler.command = ['cal', 'calc', 'calcular', 'calculadora'];
+
+export default handler;
